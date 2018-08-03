3432641661f7a00bf441541c8e0ac52f9f744b53
lucasbelizari <lucasbelizari@outlook.com>
2018-08-03 15:09:56 -0300

    First java program

---
+++ AloMundo.java
@@ -0,0 +1,12 @@
+
+public class AloMundo {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Alo, Mundo");
+    }
+
+}
+
+
+
